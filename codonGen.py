883881611bb4ce295e7e2cad92f80ccc3181b5dd
SurenSK <kumar.s.suren@gmail.com>
2024-06-30 22:05:46 -0400

    batch size

--- codonGen.py
+++ codonGen.py
@@ -22,7 +22,7 @@ def set_llm(model_id):
     model = torch.compile(model, mode="reduce-overhead", fullgraph=True)
     logLine("Model loaded.")
     
-    llm = pipeline("text-generation", model=model, tokenizer=tokenizer, BATCH_SIZE=128, max_new_tokens=256)
+    llm = pipeline("text-generation", model=model, tokenizer=tokenizer, batch_size=BATCH_SIZE, max_new_tokens=256)
     llm.tokenizer.pad_token_id = model.config.eos_token_id
     logLine("Pipeline created.")
     return llm
