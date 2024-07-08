7e81b38a08f9ec91e4547cd38c32a032c69614b4
SurenSK <kumar.s.suren@gmail.com>
2024-07-08 00:00:16 -0400

    cot

--- batched_ga.py
+++ batched_ga.py
@@ -34,14 +34,14 @@ class Sample:
         if self.fitness is not None:
             return
         for i in range(self.evalWidth):
-            self.questions[i] = llm_handler.request([f"{self.relevance_codon} {self.avoidance_codon}"], {"question1": str, "question2": str, "question3": str, "question4": str, "question5": str})
+            self.questions[i] = llm_handler.request([f"{self.relevance_codon} {self.avoidance_codon}"], {"knowledge": str, "question1": str, "question2": str, "question3": str, "question4": str, "question5": str})
     
     def setQuestions(self):
         if self.fitness is not None:
             return
         logLine(f"Setting questions for {self.relevance_codon}")
         for i in range(self.evalWidth):
-            r = self.questions[i].responses[0]
+            r = list(self.questions[i].responses[0].values())[1:]
             # logLine(f"Response Type {type(r)}: {r}")
             self.questions[i] = r
             # logLine(f"Response: {r}")
